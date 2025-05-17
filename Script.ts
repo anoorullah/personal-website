da58b470d956e1e6b214fcc919126d2f7c27db3f
Adnan Noorullah <adnan.noorullah@gmail.com>
2025-05-17 00:38:23 -0500

    Base website ready - need to add features

--- Script.ts
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const aboutText = document.getElementById('about-text');
-    if (aboutText) {
-      aboutText.textContent = "Hi, I'm Adnan Noorullah, a web developer passionate about building user-friendly web apps!";
-    }
-  });
--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', function () {
-    var aboutText = document.getElementById('about-text');
-    if (aboutText) {
-        aboutText.textContent = "Hi, I'm Adnan Noorullah, a web developer passionate about building user-friendly web apps!";
-    }
-});
