f04e42c977674bcd2cc907fcca4af8a331d4c116
Adnan Noorullah <adnan.noorullah@gmail.com>
2025-05-16 23:49:05 -0500

    Current code

---
+++ Script.ts
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const aboutText = document.getElementById('about-text');
+    if (aboutText) {
+      aboutText.textContent = "Hi, I'm Adnan Noorullah, a web developer passionate about building user-friendly web apps!";
+    }
+  });
